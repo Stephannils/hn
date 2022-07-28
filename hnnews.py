d271270a85686cd6c59cf5814c8cc32b8e4d2bb3
stephannils <steniljeh@gmail.com>
2022-07-28 22:26:59 +0200

    first commit

---
+++ hnnews.py
@@ -0,0 +1,49 @@
+import sys
+from bs4 import BeautifulSoup as bs
+import requests
+import pprint
+
+
+def get_hn_resources(num_of_pages):
+    hn_res = ''
+
+    for x in range(num_of_pages):
+        res = requests.get(f'https://news.ycombinator.com/news?p={x}')
+
+        hn_res += res.text
+
+    return hn_res
+
+
+def sort_by_votes(hn_list):
+    return sorted(hn_list, key=lambda k: k['score'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+
+    for idx, item in enumerate(links):
+        title = item.getText()
+        link = item.get('href', None)
+        has_score = subtext[idx].select('.score')
+
+        if has_score:
+            score = int(has_score[0].getText().replace(' points', ''))
+            if score >= 100:
+                hn.append({'title': title, 'link': link, 'score': score})
+    return hn
+
+
+num_of_pages = int(sys.argv[1])
+
+hn_resources = get_hn_resources(num_of_pages)
+
+soup = bs(hn_resources, 'html.parser')
+
+links = soup.select('.titlelink')
+subtext = soup.select('.subtext')
+
+
+filtered_hn = create_custom_hn(links, subtext)
+
+pprint.pprint(sort_by_votes(filtered_hn))
